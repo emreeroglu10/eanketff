56bea259d4a4484212281bb7dcc0b4255b56ca61
emreeroglu10 <emreeroglu105@gmail.com>
2025-06-03 13:09:14 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `♥ EBRAR ♥`.split('')
+const text = `♥ EBRAR  ♥`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
